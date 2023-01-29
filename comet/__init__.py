b5ceaed6487a288007a89f3013ccf4c9f8925104
Abhishek <as7122000@gmail.com>
2023-01-29 22:14:58 +0530

    chore: colors and screen size

--- comet/__init__.py
+++ comet/__init__.py
@@ -1,8 +1,8 @@
-SCREEN_WIDTH, SCREEN_HEIGHT = 800, 800
+SCREEN_WIDTH, SCREEN_HEIGHT = 1280, 720
 
 
 class color:
     WHITE = (255, 255, 255)
     BLACK = (0, 0, 0)
-    RED = (255, 0, 0)
-    OFF_WHITE = (246, 246, 246)
+    RED = (255, 100, 100)
+    OFF_WHITE = (240,) * 3
