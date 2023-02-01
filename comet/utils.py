419474310d39be43d9be02b74d2ae550572c377f
Abhishek <as7122000@gmail.com>
2023-02-01 22:29:46 +0530

    refactor: extract print function

--- comet/utils.py
+++ comet/utils.py
@@ -1,20 +1,31 @@
-class console_stats:
-    lenght_of_message = 0
+class stats:
     config = {}
 
     @classmethod
     def add(cls, **kwargs) -> None:
+        """overwrites existing keys"""
         cls.config = {**cls.config, **kwargs}
+
+    @classmethod
+    def generate_message(cls):
+        message = []
+        for k, v in cls.config.items():
+            if isinstance(v, float):
+                message.append(f"{k}:{v:5.2f}")
+            elif isinstance(v, int):
+                message.append(f"{k}:{v:5d}")
+            else:
+                message.append(f"{k}:{v}")
+        message = "   ".join(message)
+        return message
+
+
+class ConsolePrinter(stats):
+    lenght_of_message = 0
 
     @classmethod
     def write(cls) -> None:
         print("\b" * cls.lenght_of_message, end="", flush=True)
-        message = []
-        for k, v in cls.config.items():
-            if isinstance(v, float):
-                message.append(f"{k}:{v:.2f}")
-            else:
-                message.append(f"{k}:{v}")
-        message = " ".join(message)
+        message = cls.generate_message()
         cls.lenght_of_message = len(message)
         print(message, end="")
