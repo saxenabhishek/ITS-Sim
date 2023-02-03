f2476e81a483b298ad22a2de224b3bc7d748cdd8
Abhishek <as7122000@gmail.com>
2023-02-03 20:21:03 +0530

    chore: add colors from palet

--- comet/__init__.py
+++ comet/__init__.py
@@ -8,8 +8,13 @@ SCREEN_WIDTH, SCREEN_HEIGHT = 1280, 720
 DEBUG = False
 
 
-class color:
+class Color:
     WHITE = (255, 255, 255)
     BLACK = (0, 0, 0)
     RED = (255, 100, 100)
     OFF_WHITE = (240,) * 3
+    STEEL_BLUE = (92, 140, 174, 255)
+    TICKLE_ME_PINK = (240, 127, 159, 255)
+    AMBER = (255, 191, 0, 255)
+    CULTURED = (245, 245, 245, 255)
+    PRUSSIAN_BLUE = (5, 44, 74, 255)
