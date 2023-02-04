d6a6e3a10ec41ccfd069cd09771250a4ba9af08c
Abhishek <as7122000@gmail.com>
2023-02-04 12:27:35 +0530

    feat: smooth roation

--- comet/agent/follow_car.py
+++ comet/agent/follow_car.py
@@ -7,6 +7,8 @@ from comet import DEBUG
 
 
 class FollowCar(BasicCar):
+    MAX_ACCELERATION = 0.05
+
     def __init__(self, x, y, target):
         """
         Car that follows a target with constat velocity and constatn angular acceleration
@@ -23,20 +25,19 @@ class FollowCar(BasicCar):
         distance = np.sqrt(np.sum(np.subtract((self.x, self.y), (self.target.x, self.target.y)) ** 2, axis=0))
         desired_angle = 57.2958 * np.arctan2(self.y - self.target.y, self.x - self.target.x)
 
-        angular_acceleration = 5
-        console_stats.add(angular_acceleration=angular_acceleration)
+        self.accelaration = self.MAX_ACCELERATION * (1 - (self.velocity / 5) ** 4)
+
+        angular_acceleration = 0.1 * (1 - (self.angular_velocity / 10) ** 4)
+        self.angular_velocity += angular_acceleration
 
         # anything near 180 should be treated as 180
-        if (desired_angle - self.angle) % 360 < 180:
-            self.angular_velocity = min(10, self.angular_velocity + angular_acceleration)
+        if ((desired_angle % 360) - self.angle) % 360 < 180:
+            self.angle += self.angular_velocity
         else:
-            self.angular_velocity = max(-10, self.angular_velocity - angular_acceleration)
+            self.angle -= self.angular_velocity
 
-        if distance < 100:
-            self.velocity = 0
-            self.angular_velocity = 0
-        else:
-            self.velocity = 15
+        if distance < 30 and not self.target.next():
+            self.stopped = True
 
     def draw(self, win: pygame.surface.Surface):
         if DEBUG:
