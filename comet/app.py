5ba9dc5debd66d8270532d3b6dc561e5fc3924a4
Abhishek <as7122000@gmail.com>
2023-02-08 01:16:18 +0530

    feat: process trackers and add cars

--- comet/app.py
+++ comet/app.py
@@ -1,9 +1,8 @@
 import pygame
-from comet import SCREEN_HEIGHT, SCREEN_WIDTH, Color, DEBUG, FPS
-from comet.agent import IDMCar
+from comet import SCREEN_HEIGHT, SCREEN_WIDTH, FPS
 from comet.targets import Path, straight_road
 from comet.utils import WindowPrinter, stats
-
+from comet.city import City
 
 WIN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
@@ -31,8 +30,13 @@ def main():
         clock.tick(FPS)
         stats.add(t=pygame.time.get_ticks() / 1000)  # time in seconds
 
+        if len(city.trackers) != 0 and len(city.trackers) % 5 == 0:
+            city.process_trackers()
+
         for event in pygame.event.get():
             run = False if event.type == pygame.QUIT else True
+            if event.type == city.INSERT_EVENT:
+                city.consume_car_event()
 
         keys_pressed = pygame.key.get_pressed()
 
